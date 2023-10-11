8f67bd2a197d2c8cc8ba743c79fd047ad298243f
Z1pP <maxm_99@mail.ru>
2023-10-12 02:09:42 +0300

    Визуальный пиксинг

--- loader.py
+++ loader.py
@@ -1,10 +1,11 @@
 from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
-from database.sqlite import DataBase
 
+from database.sqlite import DataBase
 from core.data import config
 
-bot = Bot(token= config.bot_token)
+bot = Bot(token=config.bot_token, parse_mode=ParseMode.HTML)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
 db = DataBase('test.db')
