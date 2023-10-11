5d2bbd6207dca9e3dbef73eac997021ec67a6fc3
Z1pP <maxm_99@mail.ru>
2023-10-12 02:11:54 +0300

    Добавлены отступы

--- core/filters/admin_filter.py
+++ core/filters/admin_filter.py
@@ -4,10 +4,11 @@ from aiogram.types import Message
 from core.data.config import ADMINS
 from loader import db, dp
 
+
 class IsAdmin(BaseFilter):
     async def __call__(self, message: Message) -> bool:
         return message.from_user.id in ADMINS
-    
+
 
 class IsSuperManager(BaseFilter):
     async def __call__(self, message: Message) -> bool:
