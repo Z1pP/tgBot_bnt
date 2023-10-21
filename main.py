a3eeb8a0972d346821abd9412efe439a68bf6988
Z1pP <maxm_99@mail.ru>
2023-10-21 17:22:53 +0300

    фикс напоминалки

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import asyncio
 import logging
-from datetime import datetime
+import pytz
+
+from datetime import datetime, timedelta
 
 from loader import db, dp, bot
 from core.handlers import (
@@ -14,46 +16,52 @@ from core.utils import commands
 
 
 async def check_report_status() -> None:
-    start_time = datetime.now().replace(hour=13, minute=50, second=0, microsecond=0)
-    end_time = datetime.now().replace(hour=20, minute=0, second=0, microsecond=0)
+    # Установка часового пояса для Беларуси
+    belarus_tz = pytz.timezone('Europe/Minsk')
+
+    # Временные рамки для отправления уведомления
+    start_time = belarus_tz.localize(datetime.now().replace(hour=16, minute=50, second=0, microsecond=0))
+    end_time = belarus_tz.localize(datetime.now().replace(hour=23, minute=30, second=0, microsecond=0))
 
     while True:
-        # Проверка чтобы бот не писал с 18.00 до 2300
-        if not start_time <= datetime.now() <= end_time:
+        current_time = datetime.now(tz=belarus_tz)
+        # Проверка что не выходной день
+        if current_time.weekday() not in [0, 1, 2, 3, 4]:
+            time_sleep = 60*60*24 # Бот засыпает на сутки
+            await asyncio.sleep(time_sleep)
+            continue
+        # Проверка, чтобы бот не писал с 16:50 до 23:30
+        if not start_time.time() <= current_time.time() <= end_time.time():
+            time_sleep = start_time - current_time
+            if time_sleep.total_seconds() > 0:
+                await asyncio.sleep(time_sleep.total_seconds())
+            else:
+                # Проверка и получение времени сна для следующего дня
+                next_day_start_time = start_time + timedelta(days=1)
+                time_sleep = next_day_start_time - current_time
+                await asyncio.sleep(time_sleep.total_seconds())
+
+        # Получение списка менеджеров из БД
+        managers_list = db.get_managers()
+        
+        # Проверка что список не пустой
+        if not managers_list:
             await asyncio.sleep(1800)
             continue
 
-        current_date = datetime.now().strftime('%d.%m.%Y')
-        # Получение списка менеджеров и списка отчетов за текущий день
-        managers = db.get_managers()
+        #Получение даты в формате строки
+        current_date = datetime.now(belarus_tz).strftime('%d.%m.%Y')
         report_list = db.get_report_list_by_date(current_date)
 
-        # Две проверки на наличие менеджеров и отчетов
-        if not managers:
-            await asyncio.sleep(1800)
-            continue
-        if not report_list:
-            for manager in managers:
+        for manager in managers_list:
+            # Проверка, что менеджер отправил отчет за текущий день
+            if not any(manager[0] in report for report in report_list):
+                # Отправка уведомления если отчет не отправлен
                 await bot.send_message(
                     chat_id=manager[0],
                     text=f'Напоминаю, что вам необходимо сделать отчет за {current_date}!'
                 )
-            await asyncio.sleep(1800)
-            continue
-        
-        # Проверка что менеджер отправил отчет за текущий день
-        try:
-            for manager in managers:
-                for report in report_list:
-                    if manager[0] not in report:
-                        await bot.send_message(
-                            chat_id=manager[0],
-                            text=f'Напоминаю, что вам необходимо сделать отчет за {current_date}!'
-                        )
-                        await asyncio.sleep(1800) 
-        except Exception as e:
-            logging.error(f'Error: {str(e)}')
-
+        await asyncio.sleep(1800)
 
 
 async def main():
@@ -64,10 +72,11 @@ async def main():
         admin_handler.router,
         report_callback.router,
         admin_callback.router,
+        excel_callback.router
     )
-
     # Установка соединения с БД или создание новой
     db.create_tables_or_get_exists()
+    # Установка команд для бота
     await commands.set_commands(bot)
 
     try:
