01b3f7ec371b7a3ce51587c3e280046337dfdcbf
Max Blanc <maxm_99@mail.ru>
2024-09-09 23:25:34 +0300

    change gitingore file

--- loader.py
+++ loader.py
@@ -1,12 +1,18 @@
-from os import getenv
-
+import os
+from dotenv import load_dotenv
 from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.fsm.storage.memory import MemoryStorage
 
+load_dotenv()
+
+
 from database.sqlite import DataBase
 
-bot = Bot(token=getenv("TEST_TOKEN"), parse_mode=ParseMode.HTML)
+bot = Bot(
+    token=os.getenv("TOKEN"), default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+)
 storage = MemoryStorage()
 dp = Dispatcher(storage=storage)
-db = DataBase('test.db')
+db = DataBase("test.db")
