4f377ba8090dcc82cc54e36ce011d21ae19d9581
Z1pP <maxm_99@mail.ru>
2024-12-25 22:45:26 +0300

    Refactor Manager model: rename table to 'managers', add relationship to 'Report' model, and update import paths.

--- app/models/managers.py
+++ app/models/managers.py
@@ -1,9 +1,10 @@
 from enum import Enum
 from typing import Optional
-from sqlalchemy import String, Enum as SQLAEnum
-from sqlalchemy.orm import Mapped, mapped_column
 
-from .base import Base
+from sqlalchemy import String, Enum as SQLAEnum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import Base
 
 
 class Role(str, Enum):
@@ -13,9 +14,15 @@ class Role(str, Enum):
 
 
 class Manager(Base):
-    __tablename__ = "manager"
+    __tablename__ = "managers"
 
     tg_id: Mapped[int] = mapped_column(primary_key=True, unique=True, nullable=False)
     username: Mapped[str] = mapped_column(String(30))
     name: Mapped[Optional[str]]
     role: Mapped[Role] = mapped_column(SQLAEnum(Role), default=Role.MANAGER)
+
+    reports: Mapped[list["Report"]] = relationship(
+        back_populates="manager",
+        cascade="all, delete-orphan",
+        lazy="selectin",
+    )
