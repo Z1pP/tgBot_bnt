869defdfff7ca3cdccb641965a0152b6901f073e
Z1pP <maxm_99@mail.ru>
2025-01-01 21:46:26 +0300

    Refactor IManagersService interface by removing docstrings and updating change_name_by_tg_id method to accept updated_data dictionary instead of name string. Modify delete_manager method to return None instead of a boolean value.

--- app/services/base.py
+++ app/services/base.py
@@ -8,67 +8,24 @@ from app.schemas.reports import ReportSchemaInput, ReportSchemaOutput
 class IManagersService(ABC):
     @abstractmethod
     async def get_managers(self) -> Sequence[ManagerSchema]:
-        """
-        Получить всех менеджеров из источника данных.
-
-        Возвращает:
-            Sequence[ManagerSchema]: Список схем менеджеров
-        """
         pass
 
     @abstractmethod
     async def get_by_tg_id(self, tg_id: int) -> Optional[ManagerSchema]:
-        """
-        Получить менеджера по его Telegram ID.
-
-        Аргументы:
-            tg_id (int): ID пользователя в Telegram
-
-        Возвращает:
-            Optional[ManagerSchema]: Схема менеджера, если найден, иначе None
-        """
         pass
 
     @abstractmethod
     async def create_manager(self, schema: ManagerSchema) -> Optional[ManagerSchema]:
-        """
-        Создать нового менеджера.
-
-        Аргументы:
-            schema (ManagerSchema): Данные для создания менеджера
-
-        Возвращает:
-            ManagerSchema: Схема созданного менеджера
-        """
         pass
 
     @abstractmethod
     async def change_name_by_tg_id(
-        self, tg_id: int, name: str
+        self, tg_id: int, updated_data: dict
     ) -> Optional[ManagerSchema]:
-        """
-        Изменить имя менеджера по его Telegram ID.
-
-        Аргументы:
-            tg_id (int): ID пользователя в Telegram
-            name (str): Новое имя менеджера
-
-        Возвращает:
-            Optional[ManagerSchema]: Обновленная схема менеджера, если найден, иначе None
-        """
         pass
 
     @abstractmethod
-    async def delete_manager(self, tg_id: int) -> bool:
-        """
-        Удалить менеджера по его Telegram ID.
-
-        Аргументы:
-            tg_id (int): ID пользователя в Telegram
-
-        Возвращает:
-            bool: True, если удаление прошло успешно, иначе False
-        """
+    async def delete_manager(self, tg_id: int) -> None:
         pass
 
 
