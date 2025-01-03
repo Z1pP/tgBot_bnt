c7e4dfb2605df3b9229bd5a89bc8b17893393301
Z1pP <maxm_99@mail.ru>
2025-01-04 02:33:03 +0300

    Add create class method to ReportEntry

--- bot/entities/report.py
+++ bot/entities/report.py
@@ -11,3 +11,18 @@ class ReportEnriry:
     total_margin: Decimal
     total_revenue: Decimal
     nds: Decimal
+
+    @classmethod
+    def create(cls, report_data: dict) -> "ReportEnriry":
+        try:
+            return cls(
+                manager_tg_id=int(report_data["manager_tg_id"]),
+                total_orders=int(report_data["total_orders"]),
+                total_invoices=int(report_data["total_invoices"]),
+                paid_invoices=int(report_data["paid_invoices"]),
+                total_margin=Decimal(report_data["total_margin"]),
+                total_revenue=Decimal(report_data["total_revenue"]),
+                nds=Decimal(report_data["nds"]),
+            )
+        except (ValueError, TypeError) as e:
+            raise ValueError(f"Ошибка преобразования данных: {e}")
