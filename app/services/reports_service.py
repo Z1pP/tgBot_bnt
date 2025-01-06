789f8041bc24072bf6a94f83ac24d9fddbd8abfc
Z1pP <maxm_99@mail.ru>
2025-01-06 03:28:17 +0300

    Implement update and delete methods in ReportsService with ownership checks and logging

--- app/services/reports_service.py
+++ app/services/reports_service.py
@@ -1,11 +1,20 @@
-from app.schemas.report_schemas import ReportSchemaInput, ReportSchemaOutput
-from app.models.report_models import Report
+import logging
+
+from app.repositories.report_repository import ReportRepository
+from app.schemas.report_schemas import (
+    ReportSchemaInput,
+    ReportSchemaOutput,
+    ReportSchemaUpdate,
+)
 from app.services.base import IReportsService
 from app.services.calculation_service import FinancialCalculationService
-from app.repositories.report_repository import ReportRepository
+from app.services.permission_service import ReportPermissionMixin
 
 
-class ReportsService(IReportsService):
+logger = logging.getLogger(__name__)
+
+
+class ReportsService(IReportsService, ReportPermissionMixin):
     def __init__(
         self,
         repository: ReportRepository,
@@ -29,4 +38,35 @@ class ReportsService(IReportsService):
             return ReportSchemaOutput.from_dto(report_dto)
         except Exception as e:
             print(f"Не удалось создать отчет: {str(e)}")
+            raise
+
+    async def update_report(
+        self, report_id: int, report: ReportSchemaUpdate
+    ) -> ReportSchemaOutput:
+        try:
+            if not await self._check_report_ownership(report_id, report.manager_tg_id):
+                raise ValueError("Не досаточно прав чтобы изменить отчет")
+
+            report_data = report.model_dump(exclude_unset=True)
+            report_dto = await self._repository.update(
+                report_id=report_id, **report_data
+            )
+
+            return ReportSchemaOutput.from_dto(report_dto)
+        except Exception as e:
+            logger.error(f"Ошибка при обновлении отчета: {str(e)}")
+            raise
+
+    async def delete_report(self, report_id: int, manager_tg_id: int) -> None:
+        try:
+            if not await self._check_report_ownership(report_id, manager_tg_id):
+                raise ValueError("Не досаточно прав чтобы удалить отчет")
+
+            is_deleted = await self._repository.delete(report_id)
+            if not is_deleted:
+                logger.warning("Не удалось удалить отчет")
+                raise ValueError(f"Отчет с ID {report_id} не удален!")
+
+        except Exception as e:
+            logger.error(f"Ошибка при удалении отчета: {str(e)}")
             raise
