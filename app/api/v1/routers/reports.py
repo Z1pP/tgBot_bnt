e6ad35cfc5afd7c574db5f41e739748816652af0
Z1pP <maxm_99@mail.ru>
2025-01-06 03:26:43 +0300

    Add update and delete report endpoints to reports API
    - Introduced `update_report` endpoint for modifying existing reports using `ReportSchemaUpdate`.
    - Added `delete_report` endpoint for removing reports by ID.
    - Updated imports to include `ReportSchemaUpdate` in `reports.py`.

--- app/api/v1/routers/reports.py
+++ app/api/v1/routers/reports.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, Depends, status
 
-from app.schemas.report_schemas import ReportSchemaInput, ReportSchemaOutput
+from app.schemas.report_schemas import (
+    ReportSchemaInput,
+    ReportSchemaOutput,
+    ReportSchemaUpdate,
+)
 from app.dependencies.services import get_reports_service
 from app.services.base import IReportsService
 
@@ -34,3 +38,31 @@ async def create_report(
     service: IReportsService = Depends(get_reports_service),
 ):
     return await service.create_report(report)
+
+
+@router.put(
+    "/{report_id}",
+    response_model=ReportSchemaOutput,
+    operation_id="UpdateReport",
+    status_code=status.HTTP_200_OK,
+)
+async def update_report(
+    report_id: int,
+    report: ReportSchemaUpdate,
+    service: IReportsService = Depends(get_reports_service),
+):
+    return await service.update_report(report_id, report)
+
+
+@router.delete(
+    "/{report_id}",
+    response_model=None,
+    operation_id="DeleteReport",
+    status_code=status.HTTP_204_NO_CONTENT,
+)
+async def delete_report(
+    report_id: int,
+    manager_tg_id: int,
+    service: IReportsService = Depends(get_reports_service),
+):
+    await service.delete_report(report_id, manager_tg_id)
