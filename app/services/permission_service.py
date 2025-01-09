b6f2312296677ed22abddbfeeb43c29a30bd84ea
Z1pP <maxm_99@mail.ru>
2025-01-09 04:19:43 +0300

    Refactor ReportPermissionMixin

--- app/services/permission_service.py
+++ app/services/permission_service.py
@@ -1,25 +1,30 @@
+from typing import Optional
+
 from app.repositories.manager_repository import ManagerRepository
 from app.models.manager_models import Manager, Role
+from app.models.report_models import Report
 
 
 class ReportPermissionMixin:
+    def __init__(self, repository) -> None:
+        self._repository = repository
+
     async def _is_admin_or_super_manager(self, manager_tg_id: int) -> bool:
         manager_repository = ManagerRepository(self._repository._session)
-        manager_model = await manager_repository.get_by_tg_id(
-            manager_tg_id, return_type=Manager
+        manager_model: Optional[Manager] = await manager_repository.get_by_tg_id(
+            manager_tg_id
         )
 
-        return manager_model and manager_model.role in [Role.SUPER_MANAGER, Role.ADMIN]
+        return bool(
+            manager_model and manager_model.role in [Role.SUPER_MANAGER, Role.ADMIN]
+        )
 
     async def _check_report_ownership(
-        self, report_id: int, current_manager_tg_id: int
+        self, report: Report, current_manager_tg_id: int
     ) -> bool:
-        report_dto = await self._repository.get_by_id(report_id)
-
-        if not report_dto:
-            raise ValueError(f"Отчет с ID {report_id} не найден")
-
         return (
-            report_dto.manager_tg_id == current_manager_tg_id
-            or await self._is_admin_or_super_manager(current_manager_tg_id)
+            report.manager_tg_id == current_manager_tg_id
+            or await self._is_admin_or_super_manager(
+                manager_tg_id=current_manager_tg_id
+            )
         )
