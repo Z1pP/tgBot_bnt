ee1001f8493d4b2a9e005b6e41bee28c3d9068cb
Z1pP <maxm_99@mail.ru>
2025-01-09 04:20:49 +0300

    Refactor ReportsService to enhance error handling and permission checks

--- app/services/reports_service.py
+++ app/services/reports_service.py
@@ -1,5 +1,8 @@
 import logging
 
+from app.exceptions.permission import NoPermissionException
+from app.exceptions.repository import EntityNotFoundException
+
 from app.repositories.report_repository import ReportRepository
 from app.schemas.report_schemas import (
     ReportSchemaInput,
@@ -9,7 +12,6 @@ from app.schemas.report_schemas import (
 from app.services.base import IReportsService
 from app.services.calculation_service import FinancialCalculationService
 from app.services.permission_service import ReportPermissionMixin
-
 
 logger = logging.getLogger(__name__)
 
@@ -22,51 +24,58 @@ class ReportsService(IReportsService, ReportPermissionMixin):
     ):
         self._repository = repository
         self._finansial_service = finansial_service
+        super().__init__(repository)
 
     async def get_reports_by_manager(
         self, manager_tg_id: int
     ) -> list[ReportSchemaOutput]:
-        report_dtos = await self._repository.get_by_manager_id(manager_tg_id)
+        report_models = await self._repository.get_by_manager_id(
+            manager_tg_id=manager_tg_id
+        )
 
-        return [ReportSchemaOutput.from_dto(dto) for dto in report_dtos]
+        return [ReportSchemaOutput.from_model(model) for model in report_models]
 
-    async def create_report(self, report: ReportSchemaInput) -> ReportSchemaOutput:
-        try:
-            report_data = self._finansial_service.calculate_metrics(report)
-            report_dto = await self._repository.create(report_data)
-
-            return ReportSchemaOutput.from_dto(report_dto)
-        except Exception as e:
-            print(f"Не удалось создать отчет: {str(e)}")
-            raise
+    async def create_report(self, schema: ReportSchemaInput) -> ReportSchemaOutput:
+        report_data = self._finansial_service.calculate_metrics(report=schema)
+        report_model = await self._repository.create(
+            data=report_data.model_dump(exclude_unset=True, exclude="nds")
+        )
+        return ReportSchemaOutput.from_model(report_model)
 
     async def update_report(
-        self, report_id: int, report: ReportSchemaUpdate
+        self, report_id: int, schema: ReportSchemaUpdate
     ) -> ReportSchemaOutput:
-        try:
-            if not await self._check_report_ownership(report_id, report.manager_tg_id):
-                raise ValueError("Не досаточно прав чтобы изменить отчет")
+        existing_report = await self._repository.get_by_id(report_id=report_id)
 
-            report_data = report.model_dump(exclude_unset=True)
-            report_dto = await self._repository.update(
-                report_id=report_id, **report_data
+        if not existing_report:
+            raise EntityNotFoundException(entity="Report", entity_id=report_id)
+
+        if not await self._check_report_ownership(
+            report=existing_report, current_manager_tg_id=schema.manager_tg_id
+        ):
+            raise NoPermissionException(
+                operation="Update_report",
+                details=f"Manager {schema.manager_tg_id} does not have rights fot update report",
             )
 
-            return ReportSchemaOutput.from_dto(report_dto)
-        except Exception as e:
-            logger.error(f"Ошибка при обновлении отчета: {str(e)}")
-            raise
+        report_model = await self._repository.update(
+            model=existing_report, updated_data=schema.model_dump(exclude_unset=True)
+        )
+
+        return ReportSchemaOutput.from_model(report_model)
 
     async def delete_report(self, report_id: int, manager_tg_id: int) -> None:
-        try:
-            if not await self._check_report_ownership(report_id, manager_tg_id):
-                raise ValueError("Не досаточно прав чтобы удалить отчет")
+        existing_report = await self._repository.get_by_id(report_id=report_id)
 
-            is_deleted = await self._repository.delete(report_id)
-            if not is_deleted:
-                logger.warning("Не удалось удалить отчет")
-                raise ValueError(f"Отчет с ID {report_id} не удален!")
+        if not existing_report:
+            raise EntityNotFoundException(entity="Report", entity_id=report_id)
 
-        except Exception as e:
-            logger.error(f"Ошибка при удалении отчета: {str(e)}")
-            raise
+        if not await self._check_report_ownership(
+            report=existing_report, current_manager_tg_id=manager_tg_id
+        ):
+            raise NoPermissionException(
+                operation="Update_report",
+                details=f"Manager {manager_tg_id} does not have rights for delete report",
+            )
+
+        await self._repository.delete(model=existing_report)
